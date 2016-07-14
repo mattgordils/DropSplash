ed7bb9b140d02de80c3b43248a62e18119e74e08
Robin Willis <robincwillis@gmail.com>
2016-07-14 11:22:13 -0400

    streamline server, readme update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DropSplash
 Drop that Splash
+
+
+
+````
+~ npm install
+~ npm start
+
+http://localhost:3000
+````
+
+if stuff gets screwed up
+
+````
+~ rm -rf node_modules && npm install
+````
--- index.js
+++ index.js
@@ -1,68 +1,17 @@
-'use strict';
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
 
-var fs = require('fs');
-var path = require('path');
+const WEBPACK_PORT = 3000;
 
-var express = require('express');
-var app = express();
-
-var compress = require('compression');
-var layouts = require('express-ejs-layouts');
-
-app.set('layout');
-app.set('view engine', 'ejs');
-app.set('view options', {layout: 'layout'});
-app.set('views', path.join(process.cwd(), '/server/views'));
-
-app.use(compress());
-app.use(layouts);
-app.use('/client', express.static(path.join(process.cwd(), '/client')));
-
-app.disable('x-powered-by');
-
-var env = {
-  production: process.env.NODE_ENV === 'production'
-};
-
-if (env.production) {
-  Object.assign(env, {
-    assets: JSON.parse(fs.readFileSync(path.join(process.cwd(), 'assets.json')))
-  });
-}
-
-app.get('/*', function(req, res) {
-  res.render('index', {
-    env: env
-  });
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(WEBPACK_PORT, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  } else {
+		console.log('Listening at localhost:'+WEBPACK_PORT);
+  }
 });
-
-var port = Number(process.env.PORT || 3001);
-app.listen(port, function () {
-  console.log('server running at localhost:3001, go refresh and see magic');
-});
-
-if (env.production === false) {
-  var webpack = require('webpack');
-  var WebpackDevServer = require('webpack-dev-server');
-
-  var webpackDevConfig = require('./webpack.config.development');
-
-  new WebpackDevServer(webpack(webpackDevConfig), {
-    publicPath: '/client/',
-    contentBase: './client/',
-    inline: true,
-    hot: true,
-    stats: false,
-    historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': 'http://localhost:3001',
-      'Access-Control-Allow-Headers': 'X-Requested-With'
-    }
-  }).listen(3000, 'localhost', function (err) {
-    if (err) {
-      console.log(err);
-    }
-
-    console.log('webpack dev server listening on localhost:3000');
-  });
-}
