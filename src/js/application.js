b0dbb0c832c7da511f783d0d5942568273324756
Matt <mattgordils@gmail.com>
2016-08-08 16:37:25 -0400

    syncing up with the old one

--- src/js/application.js
+++
@@ -1,9 +0,0 @@
-import 'sass/setup/reset';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'Components/App';
-
-let state = {};
-
-ReactDOM.render(<App {...state} />, document.getElementById('root'));
