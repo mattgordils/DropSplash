2cfc444cdfc0e928ed7e480ab5495d4e738aa318
Matt Gordils <mattgordils@gmail.com>
2022-11-16 15:08:52 -0500

    get block editor working

--- src/components/BlockEditor/BlockEditor.js
+++ src/components/BlockEditor/BlockEditor.js
@@ -1,29 +1,36 @@
+import { useEffect } from 'react';
 import '@wordpress/components/build-style/style.css';
 import '@wordpress/block-editor/build-style/style.css';
 import '@wordpress/block-library/build-style/style.css';
 import '@wordpress/block-library/build-style/editor.css';
 import '@wordpress/block-library/build-style/theme.css';
 import { registerCoreBlocks } from '@wordpress/block-library';
+import { ShortcutProvider } from '@wordpress/keyboard-shortcuts';
 import {
 	BlockEditorProvider,
 	BlockList,
 	BlockTools,
 	WritingFlow,
 	ObserveTyping,
+	BlockEditorKeyboardShortcuts
 } from '@wordpress/block-editor'
 import { SlotFillProvider, Popover } from '@wordpress/components'
 import { useState } from '@wordpress/element'
 
 const BlockEditor = () => {
-  const [blocks, updateBlocks] = useState()
+  const [blocks, updateBlocks] = useState([])
+	console.log(blocks)
+
   return (
     <BlockEditorProvider
 			value={ blocks }
-			onInput={ ( blocks ) => console.log( blocks ) }
-			onChange={ ( blocks ) => console.log( blocks ) }
+			onInput={ ( blocks ) => updateBlocks( blocks ) }
+			onChange={ ( blocks ) => updateBlocks( blocks ) }
 		>
+			<ShortcutProvider>
 			<SlotFillProvider>
 				<BlockTools>
+					<BlockEditorKeyboardShortcuts.Register />
 					<WritingFlow>
 						<ObserveTyping>
 							<BlockList />
@@ -32,6 +39,7 @@ const BlockEditor = () => {
 				</BlockTools>
 				<Popover.Slot />
 			</SlotFillProvider>
+			</ShortcutProvider>
 		</BlockEditorProvider>
   )
 }
